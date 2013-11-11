7e44b8bd01a47ab728c86c963e4689f83809bbff
Nate Brennand <natebrennand@gmail.com>
2013-11-11 02:16:44 -0500

    add basic route to get all data

--- src/blueprints/__init__.py
+++ src/blueprints/__init__.py
@@ -1,3 +1,8 @@
 
 from data import data_bp
 
+from sys import path
+path.append('../')
+
+import config
+
--- src/blueprints/data.py
+++ src/blueprints/data.py
@@ -1,6 +1,22 @@
 
 from flask import Blueprint, request, g
+from rethinkdb.errors import RqlRuntimeError
+from config import make_error
+import json
+import rethinkdb
 
 data_bp = Blueprint('data routes', __name__)
 
+
+@data_bp.route('/<dataset>')
+def get_all_data(dataset):
+    """ Returns all the data from the specified dataset """
+    dataset = str(dataset)
+    print dataset
+    try:
+        data = [row for row in rethinkdb.table(dataset).run(g.rdb_conn)]
+    except RqlRuntimeError, e:
+        return make_error('DATASET_NOT_FOUND')
+
+    return json.dumps({'data': data}), 201
 
