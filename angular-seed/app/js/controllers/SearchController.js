7689267e416e26ff9671e9cfb6663c4ebe1d1bad
William Falcon <waf04@Hi-6.local>
2013-11-30 17:25:27 -0500

    basic layout for the results page

--- angular-seed/app/js/controllers/SearchController.js
+++ angular-seed/app/js/controllers/SearchController.js
@@ -4,7 +4,7 @@
 angular.module('myApp.controllers', []).
     controller('SearchController',function($scope){
 
-        //map vars
+        //map settings
         $scope.mapUrl = "img/USMap.svg";
         $scope.regions = {
             0:"midwest",
@@ -17,6 +17,18 @@ angular.module('myApp.controllers', []).
         $scope.mapWidth = null;
         $scope.activeRegion = null;
 
+        $scope.mapColors = {
+            highlighted :{
+                opacity :"1.0",
+                fillOpacity : "1.0",
+                fill : "#FFFFFF"
+            },
+            defaultState:{
+                opacity :"0.5",
+                fillOpacity : "0.5",
+                fill : "#FFFFFF"
+            }
+        };
 
         /**
          * Inits the map to a default state
@@ -37,17 +49,6 @@ angular.module('myApp.controllers', []).
         $scope.highlightMap = function(regionNumber){
 
             var regionToHighlight = $scope.regions[regionNumber];
-            var highlighted = {
-                opacity :"1.0",
-                fillOpacity : "1.0",
-                fill : "#2890FF"
-            };
-
-            var defaultState = {
-                opacity :"0.5",
-                fillOpacity : "0.5",
-                fill : "#2890FF"
-            };
 
             // deselect all rooms and highlight jsut the one we want
             for (var regionIndex in $scope.regions) {
@@ -59,15 +60,15 @@ angular.module('myApp.controllers', []).
 
                 //If proper room highlight
                 if (region == regionToHighlight) {
-                    svgElement.style.opacity          = highlighted.opacity;
-                    svgElement.style['fill-opacity']  = highlighted.fillOpacity;
-                    svgElement.style.fill             = highlighted.fill;
+                    svgElement.style.opacity          = $scope.mapColors.highlighted.opacity;
+                    svgElement.style['fill-opacity']  = $scope.mapColors.highlighted.fillOpacity;
+                    svgElement.style.fill             = $scope.mapColors.highlighted.fill;
 
                     //Otherwise set to default state
                 }else{
-                    svgElement.style.opacity          = defaultState.opacity;
-                    svgElement.style['fill-opacity']  = defaultState.fillOpacity;
-                    svgElement.style.fill             = defaultState.fill;
+                    svgElement.style.opacity          = $scope.mapColors.defaultState.opacity;
+                    svgElement.style['fill-opacity']  = $scope.mapColors.defaultState.fillOpacity;
+                    svgElement.style.fill             = $scope.mapColors.defaultState.fill;
                 }
             }
         };
