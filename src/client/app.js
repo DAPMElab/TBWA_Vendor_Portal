dd7c9fbf6b9997740dd5a94142a11cb89097fa9c
William Falcon <waf04@Hi2.local>
2013-11-30 01:02:39 -0500

    base home search layout

--- src/client/app.js
+++ src/client/app.js
@@ -3,4 +3,5 @@ angular.module('app', [
 	// list dependencies
 ]).config(['$routeProvidor', function($routeProvidor) {
 	// declare each route
+
 }]);
---
+++ src/client/controllers/home_search.js
@@ -0,0 +1,4 @@
+function homeSearchController($scope){
+    $scope.companies = [];
+
+}
