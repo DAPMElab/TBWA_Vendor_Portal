2bfa5b6003aa7f24301973dca6daa41ef3838678
William Falcon <waf04@Hi-6.local>
2013-11-30 17:34:14 -0500

    linked up routes for search results

--- angular-seed/app/js/app.js
+++ angular-seed/app/js/app.js
@@ -3,20 +3,29 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/header', {templateUrl: 'partials/header.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/mainsearch', {templateUrl: 'partials/mainsearch.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/search', {templateUrl: 'partials/search.html', controller: 'SearchController'});
+        'ngRoute',
+        'myApp.filters',
+        'myApp.services',
+        'myApp.directives',
+        'myApp.controllers'
+    ]).
+    config(['$routeProvider', function($routeProvider) {
 
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/map', {templateUrl: 'partials/map.html', controller: 'SearchController'});
+        //??
+        $routeProvider.when('/header', {templateUrl: 'partials/header.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/mainsearch', {templateUrl: 'partials/mainsearch.html', controller: 'MyCtrl1'});
+        $routeProvider.when('/search', {templateUrl: 'partials/search.html', controller: 'SearchController'});
 
-  $routeProvider.when('/sidebar', {templateUrl: 'partials/sidebar.html', controller: 'SidebarController'});
-  $routeProvider.otherwise({redirectTo: '/sidebar'});
-}]);
+        //Search results
+        $routeProvider.when('/map', {templateUrl: 'partials/map.html', controller: 'SearchController'});
+        $routeProvider.when('/category', {templateUrl: 'partials/category.html', controller: 'SearchController'});
+        $routeProvider.when('/currentResults', {templateUrl: 'partials/currentResults.html', controller: 'SearchController'});
+        $routeProvider.when('/companyDescription', {templateUrl: 'partials/companyDescription.html', controller: 'SearchController'});
+        $routeProvider.when('/companySnapshot', {templateUrl: 'partials/companySnapshot.html', controller: 'SearchController'});
+        $routeProvider.when('/companyReviews', {templateUrl: 'partials/companyReviews.html', controller: 'SearchController'});
+
+
+        //??
+        $routeProvider.when('/sidebar', {templateUrl: 'partials/sidebar.html', controller: 'SidebarController'});
+        $routeProvider.otherwise({redirectTo: '/sidebar'});
+    }]);
