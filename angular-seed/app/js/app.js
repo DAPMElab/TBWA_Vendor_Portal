efcb67f9a57dcd3a15e181c0133ba291e3c8a3b8
Justin Zhao <justin.zhao71@gmail.com>
2013-11-30 21:17:01 -0500

    Some visual fixes

--- angular-seed/app/js/app.js
+++ angular-seed/app/js/app.js
@@ -11,21 +11,17 @@ angular.module('myApp', [
     ]).
     config(['$routeProvider', function($routeProvider) {
 
-        //??
-        $routeProvider.when('/header', {templateUrl: 'partials/header.html', controller: 'MyCtrl1'});
-        $routeProvider.when('/mainsearch', {templateUrl: 'partials/mainsearch.html', controller: 'MyCtrl1'});
         $routeProvider.when('/search', {templateUrl: 'partials/search.html', controller: 'SearchController'});
 
         //Search results
         $routeProvider.when('/map', {templateUrl: 'partials/map.html', controller: 'SearchController'});
+
+        //Don't necessarily want the path, just linking up the controllers
         $routeProvider.when('/category', {templateUrl: 'partials/category.html', controller: 'SearchController'});
         $routeProvider.when('/currentResults', {templateUrl: 'partials/currentResults.html', controller: 'SearchController'});
         $routeProvider.when('/companyDescription', {templateUrl: 'partials/companyDescription.html', controller: 'SearchController'});
         $routeProvider.when('/companySnapshot', {templateUrl: 'partials/companySnapshot.html', controller: 'SearchController'});
         $routeProvider.when('/companyReviews', {templateUrl: 'partials/companyReviews.html', controller: 'SearchController'});
 
-
-        //??
-        $routeProvider.when('/sidebar', {templateUrl: 'partials/sidebar.html', controller: 'SidebarController'});
-        $routeProvider.otherwise({redirectTo: '/sidebar'});
+        $routeProvider.otherwise({redirectTo: '/search'});
     }]);
