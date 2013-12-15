6ca48c473093330848613e50756fdc6adae30dbd
williamFalcon <waf2107@columbia.edu>
2013-12-15 09:25:14 -0500

    changed routes...

--- src/client/app.js
+++ src/client/app.js
@@ -18,12 +18,12 @@ angular.module('myApp', [
         $routeProvider.when('/map',                 {templateUrl: 'client/partials/map.html',                 controller: 'SearchController'});
 
         //Don't necessarily want the path, just linking up the controllers
-        $routeProvider.when('/category', {templateUrl: 'partials/category.html', controller: 'SearchController'});
-        $routeProvider.when('/currentResults', {templateUrl: 'partials/currentResults.html', controller: 'SearchController'});
-        $routeProvider.when('/companyDescription', {templateUrl: 'partials/companyDescription.html', controller: 'SearchController'});
-        $routeProvider.when('/companySnapshot', {templateUrl: 'partials/companySnapshot.html', controller: 'SearchController'});
-        $routeProvider.when('/companyReviews', {templateUrl: 'partials/companyReviews.html', controller: 'SearchController'});
-        $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'SearchController'});
+        $routeProvider.when('/category', {templateUrl: 'client/partials/category.html', controller: 'SearchController'});
+        $routeProvider.when('/currentResults', {templateUrl: 'client/partials/currentResults.html', controller: 'SearchController'});
+        $routeProvider.when('/companyDescription', {templateUrl: 'client/partials/companyDescription.html', controller: 'SearchController'});
+        $routeProvider.when('/companySnapshot', {templateUrl: 'client/partials/companySnapshot.html', controller: 'SearchController'});
+        $routeProvider.when('/companyReviews', {templateUrl: 'client/partials/companyReviews.html', controller: 'SearchController'});
+        $routeProvider.when('/home', {templateUrl: 'client/partials/home.html', controller: 'SearchController'});
 
 
         $routeProvider.otherwise({redirectTo: '/search'});
