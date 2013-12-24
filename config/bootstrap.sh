f21dfee4db73b72441d45b2823590f02897291c5
Nate Brennand <natebrennand@gmail.com>
2013-12-24 01:33:23 -0800

    clean up unnecessary vagrant installs
    closes #35

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -6,31 +6,12 @@ apt-get -y update
 # git
 apt-get -y install git-core git
 
-# node
-apt-get -y install python-software-properties python g++ make
-add-apt-repository ppa:chris-lea/node.js
-apt-get -y update
-apt-get -y install nodejs
-
-# vim
-apt-get -y install vim
-
-# curl
-apt-get -y install curl
-
 # zsh
 apt-get -y install zsh git-core git
 git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git /home/vagrant/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 sudo -u vagrant -H cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
 chsh -s $(which zsh) vagrant
-
-# grunt 
-npm install -g grunt grunt-cli
-
-# sass
-/opt/vagrant_ruby/bin/gem install sass
-echo PATH=$PATH:/opt/vagrant_ruby/bin/sass >> /home/vagrant/.zshrc
 
 # rethinkdb
 sudo add-apt-repository -y ppa:rethinkdb/ppa
@@ -40,7 +21,6 @@ sudo apt-get -y install rethinkdb
 sudo cp /etc/rethinkdb/default.conf.sample /etc/rethinkdb/instances.d/instance1.conf
 sudo echo "bind=all" >> /etc/rethinkdb/instances.d/instance1.conf
 sudo echo "http-port=8080" >> /etc/rethinkdb/instances.d/instance1.conf
-
 
 # python
 apt-get -y install python
