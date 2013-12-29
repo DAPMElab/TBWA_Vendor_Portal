7f5ac0420db44b910ed342c5f49979a95eb67a2c
Nate Brennand <natebrennand@gmail.com>
2013-12-29 02:04:37 -0800

    wire together new admin page
    closes #37

--- src/admin/js/controllers/newAdmin.js
+++ src/admin/js/controllers/newAdmin.js
@@ -1,6 +1,24 @@
 
 
 angular.module('app.admin', [])
-.controller('NewAdminController', function () {
+.controller('NewAdminController', function ($scope, $http, $window) {
+
+  $scope.create = function () {
+    var data = { 'data': {
+      email: $scope.email,
+      password: $scope.password,
+      repeat_password : $scope.repeatPassword
+    }};
+
+    $http.post('/admin/create', data)
+      .success( function (resp) {
+        console.log(resp);
+        $window.location.href = '/admin#/';
+      })
+      .error( function (err) {
+        console.log(err);
+      });
+  };
+
 });
 
